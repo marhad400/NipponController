b6382aa4782b3e0f2fc5f8ca9b7699cf6abe823e
marhad400 <markhaddad03@gmail.com>
2024-06-13 08:30:49 -0700

    refactoring

--- stage.py
+++ stage.py
@@ -10,54 +10,46 @@ print(f"HID Name: {hid_name}")
 result, hid_handle = cmd.OpenCommanderHID(hid_name)
 print(f"Open HID result: {result}, handle: {hid_handle}")
 
+
 def setup_position():
-    setup_complete = False
-    command = "EO=3"
-    reply = ctypes.create_string_buffer(63)
-    result = cmd.SendReceiveCommanderHID(hid_handle, command, reply)
-    print(f"Command result: {result}, reply: {reply.value.decode('utf-8')}")
-    command = "X-40000"
-    x_pos = -40000
-    reply = ctypes.create_string_buffer(63)
-    result = cmd.SendReceiveCommanderHID(hid_handle, command, reply)
-    print(f"Command result: {result}, reply: {reply.value.decode('utf-8')}")
-    command = "Y1000"
-    reply = ctypes.create_string_buffer(63)
-    result = cmd.SendReceiveCommanderHID(hid_handle, command, reply)
-    print(f"Command result: {result}, reply: {reply.value.decode('utf-8')}")
+    send_command("EO=3")
+    send_command("X-40000")
+    send_command("Y1000")
 
-    while not setup_complete:
-        command = "PX"
-        reply = ctypes.create_string_buffer(63)
-        result = cmd.SendReceiveCommanderHID(hid_handle, command, reply)
-
-        if int(reply.value.decode('utf-8')) == x_pos:
-            setup_complete = True
+    setup_complete = move_complete("PX", -40000)
 
     print(f'Complete? {setup_complete}')
     return setup_complete
 
 def snake_loop():
-    reply = ctypes.create_string_buffer(63)
-    result = cmd.SendReceiveCommanderHID(hid_handle, "Y10000", reply)
-    y_pos = 10000
-    print(f"Command result: {result}, reply: {reply.value.decode('utf-8')}")
+    send_command("Y10000")
 
-    move_complete = False
-    while not move_complete:
-        command = "PY"
-        reply = ctypes.create_string_buffer(63)
-        result = cmd.SendReceiveCommanderHID(hid_handle, command, reply)
-
-        if int(reply.value.decode('utf-8')) == y_pos:
-            move_complete = True
+    move_complete("PY", 10000)
 
     if move_complete:
-        reply = ctypes.create_string_buffer(63)
-        result = cmd.SendReceiveCommanderHID(hid_handle, "X10000", reply)
-        print(f"Command result: {result}, reply: {reply.value.decode('utf-8')}")
-        
+        send_command("X10000")
+  
+def send_command(command):
+    reply = ctypes.create_string_buffer(63)
+    result = cmd.SendReceiveCommanderHID(hid_handle, command, reply)
+    print(f"Command result: {result}, reply: {reply.value.decode('utf-8')}")
 
+def get_command_reply(command):
+    reply = ctypes.create_string_buffer(63)
+    result = cmd.SendReceiveCommanderHID(hid_handle, command, reply)
+    if result:
+        reply = reply.value.decode('utf-8')
+
+    return reply
+
+def move_complete(pos_command, end_pos):
+    is_complete = False
+    while not is_complete:
+        pos = get_command_reply(pos_command)
+        if int(pos) == end_pos:
+            is_complete = True
+
+    return is_complete
 
 setup_complete = setup_position()
 
